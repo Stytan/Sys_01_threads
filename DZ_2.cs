4e23c7744c7deae893914941a2a7594ef4275b82
Stytan <stytanus@gmail.com>
2017-11-13 16:50:04 +0200

    Create DZ_2.cs

---
+++ DZ_2.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace DZ_01_2
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Bank b1 = new Bank() { Name = "Vasya", Money = 5000, Percent = 5 };
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
